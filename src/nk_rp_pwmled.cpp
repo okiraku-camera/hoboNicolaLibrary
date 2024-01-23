bc1982ab1ca793839ec87dd41954cf7f39fa2e4c
たけし ひがさ <takeshi.higasa@gmail.com>
2024-01-23 17:38:06 +0900

    change pwm led behavior

--- src/nk_rp_pwmled.cpp
+++ src/nk_rp_pwmled.cpp
@@ -1,24 +1,3 @@
-/**
- * 
-  nk_rp_pwmled.cpp RP2040 PWM LED functions.
-  Copyright (c) 2023 Takeshi Higasa
-  
-  This file is part of "Hobo-nicola keyboard and adapter library".
-
-  "Hobo-nicola keyboard and adapter" is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by the Free Software Foundation, 
-  either version 3 of the License, or (at your option) any later version.
-
-  "Hobo-nicola keyboard and adapter" is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
-  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with "Hobo-nicola keyboard and adapter".  If not, see <http://www.gnu.org/licenses/>.
-
-	hoboNicola	1.7.2   support RP2040
-*/
-
 #if defined(ARDUINO_GENERIC_RP2040)
 // select Generic_RP2040 on board config of Arduino-pico 
 #include <Arduino.h>
@@ -26,12 +5,21 @@
 
 static const int16_t pwm_wrap_value = 10000;	
 static const int16_t pwm_min_level = 100;
-static const int16_t pwm_max_level = pwm_wrap_value - 1;
-static const int16_t pwm_upper_thr_level = 7000 ;
-static const int16_t pwm_lower_thr_level = 4000 ;
+static const int16_t pwm_dimm_level = 500;
+
 static const int16_t pwm_level_step = 100;
-static const int16_t pwm_level_step_slow = 60;
-static const int16_t pwm_level_step_fast = 180;
+
+static int16_t _max_level = pwm_wrap_value - 1;
+static int16_t pwm_upper_thr_level = pwm_wrap_value / 2 ;
+static int16_t pwm_lower_thr_level = pwm_wrap_value / 4 ;
+
+
+void set_rp_pwm_max_value(int16_t val) { 
+	if (val > 0 && val < pwm_wrap_value)
+		_max_level = val;  
+}
+
+int16_t get_rp_pwm_max_value() { return _max_level; }
 
 static uint8_t pwm_gpio;
 static volatile int pwm_level = pwm_min_level;
@@ -43,34 +31,29 @@ extern "C" void isr_rp_pwm_wrap() {
 	pwm_clear_irq(pwm_gpio_to_slice_num(pwm_gpio));
 	pwm_set_gpio_level(pwm_gpio, pwm_level); 
 	if (pwm_dir == 2) {
-		if (pwm_level > 1)
-			pwm_level -= pwm_level_step;
-		else
+		pwm_level -= pwm_level_step;
+		if (pwm_level < 1)
 			pwm_level = 1;
-		return;
+			return;
 	}
 
 	if (pwm_dir == 1) {
 		if (pwm_level < pwm_upper_thr_level)
-			pwm_level += pwm_level_step;
+			pwm_level += (_max_level / 100);
 		else
-			pwm_level += pwm_level_step_fast;
-		if (pwm_level >= pwm_max_level) {
+			pwm_level += (_max_level / 50);
+		if (pwm_level >= _max_level) {
 			pwm_dir = 0;
-			pwm_level = pwm_max_level;
+			pwm_level = _max_level;
 		}
 	} else  {
-		if (pwm_level > pwm_lower_thr_level)
-			pwm_level -= pwm_level_step_fast;
-		else
-			pwm_level -= pwm_level_step_slow;	
+		pwm_level -= (_max_level / 100);
 		if (pwm_level <= pwm_min_level) {
 			pwm_dir = 1;		
 			pwm_level = pwm_min_level;
 		}
 	}
 }
-
 
 void rp_pwm_init(uint8_t gpio) {
 	pwm_gpio = gpio;
@@ -98,6 +81,18 @@ void rp_start_pwm() {
 
 void rp_stop_pwm() {
 	pwm_dir = 2;
+}
+
+void rp_pwm_dimmer() {
+	_max_level = _max_level * 0.85;
+	if (_max_level < pwm_dimm_level)
+		_max_level = pwm_dimm_level;
+}
+
+void rp_pwm_brighter() {
+	_max_level = _max_level * 1.2;
+	if (_max_level > pwm_wrap_value)
+		_max_level = pwm_wrap_value - 1;	
 }
 
 #endif
--- src/nk_rp_pwmled.h
+++ src/nk_rp_pwmled.h
@@ -1,3 +1,9 @@
 void rp_pwm_init(uint8_t gpio); 
 void rp_start_pwm();
 void rp_stop_pwm() ;
+
+void rp_pwm_dimmer();
+void rp_pwm_brighter();
+
+void set_rp_pwm_max_value(int16_t val);
+int16_t get_rp_pwm_max_value();
